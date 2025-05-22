ae9492e87982112c22484576b343fc6592c6b55b
PratikPatil <pratikpatil57432@gmail.com>
2025-05-22 10:49:26 +0530

    templateLitrals.js added

---
+++ templateLitrals.js
@@ -0,0 +1,9 @@
+// Template literals were introduced in ES6 (ES2015)
+// Use backticks (` `) instead of quotes for strings
+// Support string interpolation using ${expression}
+
+let val = "Pratik";
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+greet(val);
