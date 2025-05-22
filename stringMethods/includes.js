6ac56868a958319465bd47b645e5c677976ce7b7
PratikPatil <pratikpatil57432@gmail.com>
2025-05-22 13:47:38 +0530

    string methods added

---
+++ stringMethods/includes.js
@@ -0,0 +1,8 @@
+// The includes() method of String values performs a case-sensitive search to determine whether a given string may be found within this string,
+// returning true or false as appropriate.
+
+let str = "The quick brown fox jumps over the lazy dog";
+
+let out = str.includes("fox");
+
+console.log(out);// true
---
+++ stringMethods/startsWith.js
@@ -0,0 +1,10 @@
+//The startsWith() method of String values determines whether this string begins with the characters of a specified string,
+//  returning true or false as appropriate.
+
+let sentence = "The night is very loud";
+
+let out = sentence.startsWith("night");
+
+let res = sentence.startsWith("The");
+console.log(res);//true
+console.log(out);//false;
