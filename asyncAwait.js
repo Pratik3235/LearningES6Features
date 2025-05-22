89468d508c81049b7961aae0b62a30be8c7174af
PratikPatil <pratikpatil57432@gmail.com>
2025-05-22 11:50:11 +0530

    corrections made

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,5 +1,5 @@
 // async await is used for asynchronous operations it will not move to next line until the promise is resolved
-
+//it is introduces in ES6 and it is a better option for resolving an promise then using .then .catch
 async function fetchData() {
     let res = await fetch("https://jsonplaceholder.typicode.com/posts");
     let result =await res.json();
